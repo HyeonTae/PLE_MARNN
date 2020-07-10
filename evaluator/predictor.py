3e9271681809b75ca43527da0b1d8bb559784d95
Hyeontae <dchs504@gmail.com>
2020-07-10 17:57:13 +0900

    predictor update

--- evaluator/predictor.py
+++ evaluator/predictor.py
@@ -3,7 +3,7 @@ from torch.autograd import Variable
 
 class Predictor(object):
 
-    def __init__(self, model, src_vocab, tgt_vocab):
+    def __init__(self, model, src_vocab, tgt_vocab, pad):
         if torch.cuda.is_available():
             self.model = model.cuda()
         else:
@@ -11,6 +11,7 @@ class Predictor(object):
         self.model.eval()
         self.src_vocab = src_vocab
         self.tgt_vocab = tgt_vocab
+        self.pad = pad
 
     def get_decoder_features(self, src_seq):
         src_id_seq = torch.LongTensor([self.src_vocab.stoi[tok] for tok in src_seq]).view(1, -1)
@@ -18,9 +19,35 @@ class Predictor(object):
             src_id_seq = src_id_seq.cuda()
 
         with torch.no_grad():
-            _, _, other = self.model(src_id_seq, self.tgt_vocab, [len(src_seq)])
+            _, _, other = self.model(src_id_seq, [len(src_seq)])
 
-        #print(self.model.decoder.action_weights.cpu().numpy())
+        return other
+
+    def get_decoder_features_batch(self, src_seq):
+        lengths = []
+        
+        max_len = max([len(src_seq[i]) for i in range(len(src_seq))])
+        
+        for i, seq in zip(range(len(src_seq)),src_seq):
+            lengths.append(len(seq))
+            src_id = [self.src_vocab.stoi[tok] for tok in seq]
+            if len(seq) < max_len:
+                for j in range(max_len - len(seq)):
+                    src_id.append(self.pad)
+            #tmp = torch.LongTensor(src_id).view(1, -1)
+            
+            if i == 0:
+                src_id_seq = torch.LongTensor(src_id).view(1, -1)
+            else:
+                tmp = torch.LongTensor(src_id).view(1, -1)
+                src_id_seq = torch.cat((src_id_seq, tmp), dim=0)
+
+        if torch.cuda.is_available():
+            src_id_seq = src_id_seq.cuda()
+
+        with torch.no_grad():
+            _, _, other = self.model(src_id_seq, self.tgt_vocab, lengths)
+
         return other
 
     def predict(self, src_seq):
@@ -45,14 +72,18 @@ class Predictor(object):
         tgt_seq = [self.tgt_vocab.itos[tok] for tok in tgt_id_seq]
         return tgt_seq, tgt_att_list, encoder_outputs, action
 
-    def predict_n(self, src_seq, n=1):
-        other = self.get_decoder_features(src_seq)
+    def predict_batch(self, src_seq):
+        batch_size = len(src_seq)
+        other = self.get_decoder_features_batch(src_seq)
 
-        result = []
-        for x in range(0, int(n)):
-            length = other['topk_length'][0][x]
-            tgt_id_seq = [other['topk_sequence'][di][0, x, 0].data[0] for di in range(length)]
-            tgt_seq = [self.tgt_vocab.itos[tok] for tok in tgt_id_seq]
-            result.append(tgt_seq)
+        tgt_id_seq = list()
+        lengths = other['length']
+        for i in range(batch_size):
+            tgt_id_seq.append([other['sequence'][di][i].data[0] for di in range(lengths[i])])
 
-        return result
+        tgt_seq = list()
+        for seq in tgt_id_seq:
+            tmp = [self.tgt_vocab.itos[tok] for tok in seq]
+            tgt_seq.append(" ".join(tmp[:-1]))
+
+        return tgt_seq
